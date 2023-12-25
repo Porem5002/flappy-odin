f848d6a39c21e6f9f57a62059c19ebcc79b1c6d0
Carlos Bandeira <porem5002@gmail.com>
2023-12-25 18:02:54 +0000

    Changed optimizations to minimal

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 OUT=flappy_odin
 
 debug:
-	odin build ./src -debug -out:$(OUT)
+	odin build ./src -debug -o:none -out:$(OUT)
 
 release:
-	odin build ./src -o:speed -out:$(OUT)
+	odin build ./src -o:minimal -out:$(OUT)
 
 clean:
 	rm $(OUT)
--- build.bat
+++ build.bat
@@ -6,8 +6,8 @@ if "%1%" == "clean" (
     del *.pdb
 ) else if "%1%" == "release" (
     echo Building Release Build...
-    odin build .\src -o:speed -subsystem:windows -out:flappy_odin.exe
+    odin build .\src -o:minimal -subsystem:windows -out:flappy_odin.exe
 ) else (
     echo Building Debug Build...
-    odin build .\src -debug -out:flappy_odin.exe
+    odin build .\src -debug -o:none -out:flappy_odin.exe
 )
